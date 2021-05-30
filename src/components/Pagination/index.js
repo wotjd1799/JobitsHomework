f761a0bda4715d8df8fb1d571200b03d9c899c00
wotjd1799 <wotjd0676@gmail.com>
2021-05-30 23:57:48 +0900

    Update : axios 추가, response받아오기

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,11 +1,34 @@
+import axios from "axios"
 import SideBar from "./../SideBar"
 import * as S from "./styles"
+import React, { useState, useEffect } from 'react';
+import { request } from "./../../utils/axios/axios"
 
 const Pagination = () => {
 
+  const [Data, setData] = useState([]);
+  const [PageNum, setPageNum] = useState(1);
+  const [MaxPageNum, setMaxPageNum] = useState(1);
+
+  useEffect(() => {
+    request("GET",`/page?page=${PageNum}`)
+    .then((res) => {
+      setMaxPageNum(res.maxPage);
+      setData(res.data);
+    })
+  },[PageNum]);
+  useEffect(()=>{
+    console.log(Data)
+  },[Data])
+
   return (
     <>
-      <SideBar paginationButtonColor={true} />
+      <SideBar paginationButtonColor={true}/>
+      {Data.map((v,i) => {
+          return(
+            <S.Container key={i}>{v.name}</S.Container>
+          )
+      })}
     </>
   );
 }
--- src/components/Pagination/styles.js
+++ src/components/Pagination/styles.js
@@ -1,1 +1,7 @@
 import styled from "styled-components"
+
+export const Container = styled.div`
+  width: 300px;
+  height: 300px;
+  margin-left: 300px;
+`
