d2e652df379ea1a650930df79191d1886841b03e
wotjd1799 <wotjd0676@gmail.com>
2021-06-03 23:47:26 +0900

    Update : 기본 페이지 변경

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import MainRouter from './router/MainRouter';
+import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom';
+import SideBar from './components/SideBar'
+import Pagination from "./components/Pagination";
+import InfinityScroll from "./components/InfinityScroll"
+import { useEffect } from 'react';
+
+const First = () => {
+  const history = useHistory();
+  useEffect(() => history.push("/pagination/1")); 
+  return (<></>)
+}
 
 function App() {
   return (
     <>
       <Router>
+        <SideBar paginationButtonColor={true}></SideBar>
         <Switch>
-          <Route path='/' component={ MainRouter }/>
+          <Route exact path="/" component={First} />
+          <Route exact path="/pagination/:id" component={Pagination} />
+          <Route exact path="/infinity-scroll" component={InfinityScroll} />
         </Switch>
       </Router>
     </>
