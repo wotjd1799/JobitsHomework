4716d6dccf9f0da82ccd5f19a6577e1575ae78c8
wotjd1799 <wotjd0676@gmail.com>
2021-06-03 23:54:07 +0900

    Update : link 형태로 변경

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,21 +1,17 @@
 import * as S from "./styles"
-import { Link } from "react-router-dom"
 
-const SideBar = ({ paginationButtonColor, infinityButtonColor }) => {
+const SideBar = () => {
+
 
   return (
     <>
       <S.SideBar>
-        <Link to="/">
-          <S.PaginationButton color={paginationButtonColor}>
+          <S.Button to="/pagination/1" activeStyle={S.ActiveStyle}>
             페이지 네이션
-          </S.PaginationButton>
-        </Link>
-        <Link to="/infinity-scroll">
-          <S.InfinityScrollButton color={infinityButtonColor}>
+          </S.Button>
+          <S.Button to="/infinity-scroll" activeStyle={S.ActiveStyle}>
             무한 스크롤
-          </S.InfinityScrollButton>
-        </Link>
+          </S.Button>
       </S.SideBar>
     </>
   );
--- src/components/SideBar/styles.js
+++ src/components/SideBar/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { NavLink } from 'react-router-dom'
 
 export const SideBar = styled.div`
   display: inline-block;
@@ -7,25 +8,20 @@ export const SideBar = styled.div`
   background-color: #2F2F2F;
   width: 300px;
   height: 100%;
+  padding-top: 150px;
 `;
 
-export const PaginationButton = styled.button`
-  margin-top: 150px;
+export const Button = styled(NavLink)`
   width: 100%;
-  height: 50px;
-  outline: none;
-  background-color: ${(props) => (props.color ? "#009D00" : "#2F2F2F")};
-  border: none;
   color: white;
   font-size: 15px;
-`;
+  text-decoration: none;
+  padding: 18px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
-export const InfinityScrollButton = styled.button`
-  width: 100%;
-  height: 50px;
-  outline: none;
-  background-color: ${(props) => (props.color ? "#009D00" : "#2F2F2F")};
-  border: none;
-  color: white;
-  font-size: 15px;
-`;
+export const ActiveStyle = {
+  background: "#009D00"
+}
