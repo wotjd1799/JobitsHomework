df6138c684a86d582e60e5f93135e39cec358025
wotjd1799 <wotjd0676@gmail.com>
2021-06-03 23:52:33 +0900

    Update : container 구조 작성, id값 받아오기

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,34 +1,54 @@
-import axios from "axios"
-import SideBar from "./../SideBar"
 import * as S from "./styles"
 import React, { useState, useEffect } from 'react';
 import { request } from "./../../utils/axios/axios"
 
-const Pagination = () => {
-
+const Pagination = (props) => {
   const [Data, setData] = useState([]);
   const [PageNum, setPageNum] = useState(1);
   const [MaxPageNum, setMaxPageNum] = useState(1);
+  const {id} = props.match.params;
 
   useEffect(() => {
-    request("GET",`/page?page=${PageNum}`)
+    request("GET",`/page?page=${id}`)
     .then((res) => {
       setMaxPageNum(res.maxPage);
-      setData(res.data);
-    })
-  },[PageNum]);
-  useEffect(()=>{
-    console.log(Data)
-  },[Data])
+      setData(res.data);  
+    });
+  }, [PageNum]);
+  
+
+  useEffect(() => {
+    sessionStorage.setItem('id',id);
+    console.log(id)
+  }, [props.match.params]);
 
   return (
     <>
-      <SideBar paginationButtonColor={true}/>
-      {Data.map((v,i) => {
-          return(
-            <S.Container key={i}>{v.name}</S.Container>
-          )
-      })}
+      <S.Body>
+        <S.Box>
+          <S.Containers>
+            <S.Subtitle>채용 의뢰</S.Subtitle>
+            <S.Line></S.Line>
+            <S.Header>
+              <S.Header1>기업 명</S.Header1>
+              <S.Header2>등록일</S.Header2>
+            </S.Header>
+            <S.ScrollBox>
+              {Data.map((v,i) => {
+                return(
+                  <S.Container key={i}>
+                    <S.Name>{v.name}</S.Name>
+                    <S.Date>{v.createdAt}</S.Date>
+                  </S.Container>
+                )
+              })}
+            </S.ScrollBox>
+          </S.Containers>
+        </S.Box>
+        <S.Pagination>
+          <></>
+        </S.Pagination>
+      </S.Body>
     </>
   );
 }
